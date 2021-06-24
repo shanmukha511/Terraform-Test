1ab98a6b6de988d7a80546a26e527d3df8a06c29
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2021-06-24 16:45:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.73.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "firm-region-314807"
+}
 resource "google_compute_firewall" "default" {
   name    = "test-firewall"
   network = "default"
