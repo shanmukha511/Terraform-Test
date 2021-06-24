d62d595add2cef022d40b4bc953dc93ae5c38a1c
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2021-06-24 18:27:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,26 @@ resource "google_compute_firewall" "default" {
     protocol = "icmp"
   }
 }
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-standard-2"
+  zone         = "us-central1-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
